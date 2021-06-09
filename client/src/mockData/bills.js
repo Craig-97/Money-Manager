87952599e919d45f28313bc6125e5dda731130fe
Craig-97 <eoschazmr@googlemail.com>
2021-06-09 22:38:32 +0100

    Update styles and gitignore

--- client/src/mockData/bills.js
+++
@@ -1,7 +0,0 @@
-export const bills = [
-  { name: "Netflix", amount: 6 },
-  { name: "Discord Nitro", amount: 8 },
-  { name: "Spotify", amount: 12 },
-  { name: "Phone", amount: 18 },
-  { name: "Rent", amount: 200 },
-];
--- client/src/mockData/paymentsDue.js
+++
@@ -1,6 +0,0 @@
-export const paymentsDue = [
-  { name: "Wheel caps", amount: 4 },
-  { name: "Discord Nitro", amount: 8 },
-  { name: "Phone", amount: 18 },
-  { name: "Glasses", amount: 155 },
-];
