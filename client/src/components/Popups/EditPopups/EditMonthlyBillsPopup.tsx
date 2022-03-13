306a41c8efcb686b8a341a836d2f7b705c4f3816
Craig-97 <eoschazmr@googlemail.com>
2022-03-13 16:37:31 +0000

    - Automatically update values when bill is marked as paid

--- client/src/components/Popups/EditPopups/EditMonthlyBillsPopup.tsx
+++ client/src/components/Popups/EditPopups/EditMonthlyBillsPopup.tsx
@@ -1,7 +1,14 @@
 import { useMutation } from '@apollo/client';
 import { DispatchWithoutAction } from 'react';
-import { deleteBillCache, DELETE_BILL_MUTATION, EDIT_BILL_MUTATION } from '../../../graphql';
-import { Bill } from '../../../interfaces';
+import {
+  deleteBillCache,
+  DELETE_BILL_MUTATION,
+  editAccountCache,
+  EDIT_ACCOUNT_MUTATION,
+  EDIT_BILL_MUTATION
+} from '../../../graphql';
+import { Account, Bill } from '../../../interfaces';
+import { useAccountContext } from '../../../state';
 import { MonthlyBillsPopup } from '../PopupForms';
 
 interface EditMonthlyBillsPopupProps {
@@ -15,21 +22,53 @@ export const EditMonthlyBillsPopup = ({
   close,
   selectedBill
 }: EditMonthlyBillsPopupProps) => {
-  const { id, name, amount, paid }: Bill = selectedBill;
+  const {
+    state: { account }
+  } = useAccountContext();
+  const { bankBalance, id }: Account = account;
+  const { id: billId, name, amount, paid }: Bill = selectedBill;
 
-  const [editBill] = useMutation(EDIT_BILL_MUTATION);
+  const [editBill] = useMutation(EDIT_BILL_MUTATION, {
+    onCompleted: data => onEditBillCompleted(data)
+  });
 
   const editNewBill = (bill: Bill) => {
     editBill({
-      variables: { id, bill }
+      variables: { id: billId, bill }
     });
+  };
+
+  const [editAccount] = useMutation(EDIT_ACCOUNT_MUTATION);
+
+  const onEditBillCompleted = (response: any) => {
+    const {
+      editBill: { bill, success }
+    } = response;
+
+    if (success && !selectedBill?.paid && bill?.paid) {
+      const newBalance = bankBalance - bill?.amount;
+      if (!isNaN(newBalance)) {
+        // Updates bankBalance automatically when bill is marked as paid
+        editAccount({
+          variables: { id, account: { bankBalance: newBalance } },
+          update: (
+            cache,
+            {
+              data: {
+                editAccount: { account }
+              }
+            }
+          ) => editAccountCache(cache, account)
+        });
+      }
+    }
   };
 
   const [deleteBill] = useMutation(DELETE_BILL_MUTATION);
 
   const deleteSelectedBill = () => {
     deleteBill({
-      variables: { id },
+      variables: { id: billId },
       update: (
         cache,
         {
