7d8e35af26428ae0f5f4ac94aa9e92221ad23aca
Craig-97 <eoschazmr@googlemail.com>
2022-06-19 22:07:50 +0100

    clear apollo store on logout

--- src/components/Header/Buttons/LogoutButton.tsx
+++ src/components/Header/Buttons/LogoutButton.tsx
@@ -1,14 +1,17 @@
 import Logout from '@mui/icons-material/Logout';
+import { useApolloClient } from '@apollo/client';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import { useNavigate } from 'react-router-dom';
 
 export const LogoutButton = () => {
   const navigate = useNavigate();
+  const client = useApolloClient();
 
   const logout = () => {
     localStorage.removeItem('token');
     navigate('/login');
+    client.clearStore();
   };
 
   return (
