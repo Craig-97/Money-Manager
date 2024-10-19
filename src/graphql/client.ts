8c95d49f90700d51920af641181d48dfc6a2d898
Craig <41014229+Craig-97@users.noreply.github.com>
2024-10-19 03:51:22 +0100

    CRA to Vite (#26)
    * CRA to Vite
    
    * Update package json

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,5 +1,4 @@
-import { onError } from '@apollo/link-error';
-import { ApolloClient, createHttpLink, from, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 /* Custom merge function for account queries */
@@ -8,30 +7,25 @@ const cache = new InMemoryCache({
     Account: {
       fields: {
         bills: {
-          merge: (_old, incoming) => incoming
+          merge: (old, incoming) => incoming
         },
         oneOffPayments: {
-          merge: (_old, incoming) => incoming
+          merge: (old, incoming) => incoming
         },
         notes: {
-          merge: (_old, incoming) => incoming
+          merge: (old, incoming) => incoming
         }
       }
     }
   }
 });
 
-/* Setting API URL depending on environment */
-const uri =
-  process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_PROD_API_URL
-    : process.env.REACT_APP_DEV_API_URL;
+const uri = import.meta.env.PROD ? import.meta.env.VITE_PROD_API_URL : import.meta.env.VITE_DEV_API_URL;
 
 const httpLink = createHttpLink({
   uri
 });
 
-/* Authorization token automatically added to every GQL request if found in local storage */
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem('token');
@@ -44,18 +38,7 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-/* Log any problem and error responses coming from the backend */
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Locations:[{Line: ${locations?.[0].line}, Column: ${locations?.[0].column}}], Path: ${path}`
-      )
-    );
-  if (networkError) console.log(`[Network error]: ${networkError}`);
-});
-
 export const client = new ApolloClient({
-  link: from([authLink, errorLink, httpLink]),
+  link: authLink.concat(httpLink),
   cache
 });
