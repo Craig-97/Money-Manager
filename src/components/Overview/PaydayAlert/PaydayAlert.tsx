decaaa34840bc79142cbea2afc4157bbbb1b56ab
Craig Baxter <eoschazmr@googlemail.com>
2024-10-26 02:23:15 +0100

    feat: Add default payday config for now

--- src/components/Overview/PaydayAlert/PaydayAlert.tsx
+++ src/components/Overview/PaydayAlert/PaydayAlert.tsx
@@ -1,11 +1,12 @@
 import { Alert } from '@mui/material';
 import { useState } from 'react';
-import { PaydayConfig, PaydayType } from '~/types';
+import { PaydayConfig, PaydayType, PayFrequency } from '~/types';
 import { formatFullDate, getPayday } from '~/utils';
 
 export const PaydayAlert = () => {
   const config: PaydayConfig = {
-    type: PaydayType.SPECIFIC_DATE,
+    type: PaydayType.SET_DAY_OR_BEFORE,
+    frequency: PayFrequency.MONTHLY,
     dayOfMonth: 28
   };
   const { payday, isPayday } = getPayday(new Date(), config);
