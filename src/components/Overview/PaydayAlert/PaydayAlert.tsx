e621c0d73857a6d6477e7a69a85dcfc0d869eca4
Craig Baxter <eoschazmr@googlemail.com>
2024-10-31 23:37:59 +0000

    feat: Add session storage to store payday alert closed state

--- src/components/Overview/PaydayAlert/PaydayAlert.tsx
+++ src/components/Overview/PaydayAlert/PaydayAlert.tsx
@@ -3,9 +3,20 @@ import { useState } from 'react';
 import { useGetPayday } from '~/hooks';
 import { formatFullDate } from '~/utils';
 
+const PAYDAY_ALERT_CLOSED_KEY = 'paydayAlertClosed';
+
+const getAlertClosedStatus = () => sessionStorage.getItem(PAYDAY_ALERT_CLOSED_KEY) === 'true';
+const setAlertClosedStatus = (status: boolean) =>
+  sessionStorage.setItem(PAYDAY_ALERT_CLOSED_KEY, status.toString());
+
 export const PaydayAlert = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(() => !getAlertClosedStatus());
   const { paydayInfo, error } = useGetPayday();
+
+  const handleClose = () => {
+    setIsOpen(false);
+    setAlertClosedStatus(true);
+  };
 
   if (!isOpen || error || paydayInfo.payday === null) {
     return null;
@@ -14,7 +25,7 @@ export const PaydayAlert = () => {
   const { payday, isPayday } = paydayInfo;
 
   return (
-    <Alert className="alert" severity="info" onClose={() => setIsOpen(false)}>
+    <Alert className="alert" severity="info" onClose={handleClose}>
       <strong>{isPayday ? `TODAY IS PAYDAY` : `NEXT PAYDAY IS ${formatFullDate(payday)}`}</strong>
     </Alert>
   );
